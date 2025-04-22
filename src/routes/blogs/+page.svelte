0418fc17bb0189d973d9e19cf503093d20ed358a
Admin <admin@MacBook-Pro.local>
2025-04-22 18:28:13 +0530

    Blogupdate2

--- src/routes/blogs/+page.svelte
+++ src/routes/blogs/+page.svelte
@@ -122,7 +122,7 @@
 
   .featured-grid {
     display: grid;
-    grid-template-columns: 60% 40%;
+    grid-template-columns: 55% 42%;
     grid-template-rows: repeat(2, 1fr);
     gap: 1.5rem;
     margin-bottom: 3rem;
