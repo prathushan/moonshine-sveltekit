9a2f5d341b928a35473aca6c93f830d3254a0cfd
v-i-nay <vinay@softscrips.net>
2025-04-23 19:41:06 +0530

    change the blog individual slug

--- src/Components/Blogs.svelte
+++ src/Components/Blogs.svelte
@@ -43,7 +43,7 @@
 
   <div class="lt-asym-grid">
     {#each blogs.slice(0, limit) as blog, i}
-      <a href={`/latest-trends/${blog.slug.current}`} class={`lt-card ${i === 0 ? 'large' : 'small'}`}>
+      <a href={`/blogs/${blog.slug.current}`} class={`lt-card ${i === 0 ? 'large' : 'small'}`}>
         <img src={blog.mainImage.asset.url} alt={blog.title} class="lt-image"  loading="lazy" />
 
         <div class="lt-meta">
